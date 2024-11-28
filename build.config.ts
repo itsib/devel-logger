e275de68608c6b336a4308b3bb4d3ee002e79676
Sergey Krainov <itsib.su@gmail.com>
2024-11-28 22:39:29 +0700

    Try fixed import

--- build.config.ts
+++ build.config.ts
@@ -18,6 +18,7 @@ export default defineBuildConfig([
         format: 'esm',
       },
     },
+    declaration: true,
     outDir: dist,
   },
   {
@@ -33,7 +34,7 @@ export default defineBuildConfig([
     rollup: {
       output: {
         format: 'cjs',
-        entryFileNames: 'index.js',
+        entryFileNames: 'index.cjs',
       },
       esbuild: {
         minify: true,
@@ -44,16 +45,16 @@ export default defineBuildConfig([
         const pkgRaw = await fs.readFile(path.resolve(root, 'package.json'), 'utf8');
         const pkg = {
           ...JSON.parse(pkgRaw),
-          main: 'index.js',
-          module: 'index.mjs',
-          types: 'index.dts',
+          main: 'index.cjs',
+          types: 'index.d.ts',
+          type: 'module',
           files: ['*'],
           exports: {
             './package.json': './package.json',
             '.': {
-              types: './index.d.ts',
+              types: './index.d.mts',
               import: './index.mjs',
-              require: './index.js',
+              require: './index.cjs',
             },
           },
           scripts: {},
@@ -64,7 +65,7 @@ export default defineBuildConfig([
 
         await fs.writeFile(path.join(dist, 'package.json'), JSON.stringify(pkg, null, '  '), 'utf8');
 
-        await fs.rm(path.resolve(dist, 'index.d.mts'), { force: true });
+        // await fs.rm(path.resolve(dist, 'index.d.mts'), { force: true });
 
         await fs.cp(path.resolve(root, 'README.md'), path.resolve(dist, 'README.md'));
         await fs.cp(path.resolve(root, 'LICENSE'), path.resolve(dist, 'LICENSE'));
